9d4f7156a4456fbf44585960bbb71633bcc2bf0d
divyasri789 <67066368+divyasri987@users.noreply.github.com>
2021-10-29 21:38:09 +0530

    Check if Linked List is Palindrome

---
+++ Check if Linked List is Palindrome.java
@@ -0,0 +1,130 @@
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+
+class Is_LinkedList_Palindrom
+{
+	 Node head;  
+	 Node tail;
+	
+	/* Function to print linked list */
+    void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+ 
+    /* Inserts a new Node at front of the list. */
+    public void addToTheLast(Node node) 
+	{
+
+		if (head == null) 
+		{
+			head = node;
+			tail = node;
+		} else 
+		{
+		    tail.next = node;
+		    tail = node;
+		}
+    }
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		 
+		while(t>0)
+        {
+			int n = sc.nextInt();
+			//int k = sc.nextInt();
+			Is_LinkedList_Palindrom llist = new Is_LinkedList_Palindrom();
+			//int n=Integer.parseInt(br.readLine());
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            Node tail = head;
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+			    tail.next = new Node(a);
+			    tail = tail.next;
+			}
+			
+			Solution g = new Solution();
+			if(g.isPalindrome(head) == true)
+			    System.out.println(1);
+		    else
+			    System.out.println(0);
+			t--;
+		}
+		
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+/* Structure of class Node is
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}*/
+
+class Solution
+{
+    //Function to check whether the list is palindrome.
+    boolean isPalindrome(Node head) {
+        Node curr1=head, curr2=null, temp;
+        int size=0, mid=0;
+        
+        while(curr1!=null){
+            temp=new Node(curr1.data);
+            if(curr2==null){
+                curr2=temp;
+            }else{
+                temp.next=curr2;
+                curr2=temp;
+            }
+            curr1=curr1.next;
+            size++;
+        }
+        
+        curr1=head;
+       
+        while(curr1!=null && curr2!=null){
+            if( curr1.data!=curr2.data){
+                return false;
+            }else{
+                curr1=curr1.next;
+                curr2=curr2.next;
+                mid++;
+            }
+        }
+        return true;
+    }
+}
