f1e7669a3e16ca645d332ed780eb2b947d30597e
SANEEP P P <61908137+saneep-debug@users.noreply.github.com>
2021-10-30 20:13:22 +0530

    Program to concatenate two strings using pointer

---
+++ Program to concatenate two strings using pointer.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+
+void concatenator(char *str, char *substr) {
+
+    while (*str)
+        str++;
+
+    while (*substr) {
+        *str = *substr;
+        substr++;
+        str++;
+    }
+
+    *str = '\0';
+}
+
+int main() {
+
+    char str[100], substr[100];
+
+    printf("Enter the source string : ");
+    gets(str);
+
+    printf("\nEnter string to concatenate : ");
+    gets(substr);
+
+    concatenator(str, substr);
+
+    printf("String after concatenation is \"%s\"\n", str);
+
+    return 0;
+}
